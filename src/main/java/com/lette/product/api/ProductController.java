1e110a714e44b98da692f6ab4112928013cd0bee
cjhlette <cjh514@gmail.com>
2019-02-16 21:25:26 +0900

    spring msa 준비중.

---
+++ src/main/java/com/lette/product/api/ProductController.java
@@ -0,0 +1,25 @@
+package com.lette.product.api;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/products")
+public class ProductController {
+
+    @RequestMapping(path = "{productId}", method = RequestMethod.GET)
+    public String getProductInfo(@PathVariable String productId) {
+
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
+//        throw new RuntimeException("I/O Exception");
+    }
+
+}
